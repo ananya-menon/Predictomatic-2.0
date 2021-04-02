374a0dffef2244cc24c27236920c1ceb63b18900
Ananya M Menon <60038609+ananya-menon@users.noreply.github.com>
2021-04-02 21:36:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+model = pickle.load(open('PCOS.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('PCOS.html')
+
+
+@app.route('/predict', methods = ['GET', 'POST'])
+def predict():
+
+    int_features = [int (x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = int (prediction[0])
+    print (output)
+    
+    if output == 0:
+        return render_template('PCOS.html', prediction_text= 'PCOS : No')
+    else:
+        return render_template('PCOS.html', prediction_text= 'PCOS : Yes')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
